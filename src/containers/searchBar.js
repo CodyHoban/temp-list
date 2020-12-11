ecd1f6fa141e53a3caba88aa2e12b2a3ac1b8af3
Cody <hoban.j.cody@gmail.com>
2020-12-11 13:05:19 -0500

    finishing app / adding graphs

--- src/containers/searchBar.js
+++ src/containers/searchBar.js
@@ -14,7 +14,6 @@ class SearchBar extends React.Component {
     }
 
     onInputChange(event) {
-        console.log(event.target.value);
         this.setState({ term: event.target.value });
     }
 
